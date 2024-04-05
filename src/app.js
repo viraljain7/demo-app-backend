0a5115ff5e3376678ba5f67bac36d301630bc7af
viraljain7 <veerljain1234@gmail.com>
2024-04-05 11:04:36 +0530

    all setup configured setup

--- src/app.js
+++ src/app.js
@@ -13,5 +13,13 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+//=> url: http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter)
+
 export { app };
 
